4ae3dabb0a9e0a9ab120775c78f50139bab2657a
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-03 20:32:17 +0530

    input.java

---
+++ INTRO TO JAVA/Code/input.java
@@ -0,0 +1,9 @@
+package com.koushik;
+
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // To take input from the user
+}
