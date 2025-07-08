9aa73cf0cfb4d2604bf5fa222be0a07c4a2a5698
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-08 21:47:44 +0530

    SMALLEST OF 3 NUMBERS.java

---
+++ CONDITIONAL STATEMENTS QNA/SMALLEST OF 3 NUMBERS.java
@@ -0,0 +1,22 @@
+// WRITE A CODE TO GET SMALLEST OF THE THREE NUMBERS
+import java.util.Scanner;
+
+public class SmallestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+        int smallest = a;
+        if (b < smallest) {
+            smallest = b;
+        }
+        if (c < smallest) {
+            smallest = c;
+        }
+        System.out.println("Smallest number is: " + smallest);
+    }
+}
