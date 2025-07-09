870309be98d8cca1934b7e3961d795ba94fb438b
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-09 21:44:13 +0530

    RETURN CALL.java

---
+++ 06. METHODS || FUNCTIONS QNA/METHOD CALL & RETURN/RETURN CALL.java
@@ -0,0 +1,9 @@
+public class Main {
+    static int myMethod(int x) {
+        return 5 + x; // returns 5 + given value
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myMethod(3)); // prints 5 + 3 = 8
+    }
+}
