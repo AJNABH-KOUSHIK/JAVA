3cbbc2b6a015d046e1c93ca53cd59e806ee24195
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-09 15:21:09 +0530

    06. ODD NUMBERS FROM 5 TO 50.java

---
+++ 05. LOOPS/06. ODD NUMBERS FROM 5 TO 50.java
@@ -0,0 +1,8 @@
+// WRITE A CODE TO PRINT ODD NUMBERS FROM 5 TO 50
+public class PrintOddFrom5To50 {
+    public static void main(String[] args) {
+        for (int i = 5; i <= 50; i++) {
+            if (i % 2 != 0) System.out.print(i + " ");
+        }
+    }
+}
