9c04ad7e3b497eedc1ee1af74f6b446d472ff49a
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-09 21:05:53 +0530

    02. PRINT MATHS , PHYSICS , CHEMISTRY MARKS OF A STUDENT.java

---
+++ 07. ARRAYS QNA/02. PRINT MATHS , PHYSICS , CHEMISTRY MARKS OF A STUDENT.java
@@ -0,0 +1,14 @@
+// WRITE A CODE TO PRINT TO PRINT MATHS , PHYSICS , CHEMISTRY MARKS OF A STUDENT 
+import java.util.Scanner;
+
+public class Koushik{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] marks = {100,98,99};
+        System.out.println("Enter Math, Physics, Chemistry marks:");
+
+        System.out.println("Math: " + marks[0]);
+        System.out.println("Physics: " + marks[1]);
+        System.out.println("Chemistry: " + marks[2]);
+    }
+}
