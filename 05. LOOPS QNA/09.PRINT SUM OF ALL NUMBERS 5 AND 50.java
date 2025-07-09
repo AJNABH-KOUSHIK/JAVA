fc8135a82dd96f93850ab2ffc4ee9e5c2ce802e8
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-09 15:38:54 +0530

    09.PRINT SUM OF ALL NUMBERS 5 AND 50.java

---
+++ 05. LOOPS QNA/09.PRINT SUM OF ALL NUMBERS 5 AND 50.java
@@ -0,0 +1,8 @@
+// WRITE A CODE TO PRINT SUM OF ALL NUMBERS 5 AND 50
+public class SumFrom5To50 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 5; i <= 50; i++) sum += i;
+        System.out.println("Sum = " + sum);
+    }
+}
