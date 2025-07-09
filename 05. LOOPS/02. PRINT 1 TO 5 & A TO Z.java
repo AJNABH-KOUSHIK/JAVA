5b7f4d657a503cbacdbaf04ec19c782b1f33f708
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-09 15:08:45 +0530

    02. PRINT 1 TO 5 & A TO Z.java

---
+++ 05. LOOPS/02. PRINT 1 TO 5 & A TO Z.java
@@ -0,0 +1,12 @@
+// WRITE A CODE TO PRINT 1 TO 5 & A TO Z USING INCREMENT AND DECREMENT OPERTAOR
+public class Q2_Print1To5AndAToZ {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            System.out.print(ch + " ");
+        }
+    }
+}
