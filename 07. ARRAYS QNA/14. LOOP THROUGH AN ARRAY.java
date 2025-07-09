e8d11918880bf9de22451d922f18e60b7e3d1a0d
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-09 22:30:05 +0530

    14. LOOP THROUGH AN ARRAY.java

---
+++ 07. ARRAYS QNA/14. LOOP THROUGH AN ARRAY.java
@@ -0,0 +1,17 @@
+// LOOP THROUGH AN ARRAY
+public class LoopArrayExample {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+        // Loop through the array using a for loop
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]); // Prints each car name
+        }
+    }
+}
+
+// Output:
+// Volvo
+// BMW
+// Ford
+// Mazda
