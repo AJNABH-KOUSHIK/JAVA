ee5e13c88d7ea9a809c1276fba93c79ce4b592d6
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-09 20:41:46 +0530

    01. PRINT HELLO USING METHOD.java

---
+++ 06. METHODS || FUNCTIONS QNA/01. PRINT HELLO USING METHOD.java
@@ -0,0 +1,10 @@
+// WRITE A CODE TO PRINT HELLO USING METHOD
+public class Q16_HelloFunction {
+    static void sayHello() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
