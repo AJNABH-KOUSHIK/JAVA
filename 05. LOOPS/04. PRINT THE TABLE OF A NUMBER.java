a5d3dc3616d143054b18818c610915d2fd449c10
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-09 15:14:33 +0530

    04. PRINT THE TABLE OF A NUMBER.java

---
+++ 05. LOOPS/04. PRINT THE TABLE OF A NUMBER.java
@@ -0,0 +1,12 @@
+// WRITE A CODE TO PRINT THE TABLE OF A NUMBER
+import java.util.Scanner;
+
+public class Q5_PrintTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
