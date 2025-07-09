9f00c2bde90ad1becf2f45ed5256d4fd116beb55
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-09 21:42:04 +0530

    EXAMPLE OF OVERLOADING.java

---
+++ 06. METHODS || FUNCTIONS QNA/METHOD OVERLOADING/EXAMPLE OF OVERLOADING.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        fun(67);
+        fun("vijju");
+    }
+
+    static void fun(int a) {
+        System.out.println(a);
+    }
+
+    static void fun(String name) {
+        System.out.println(name);
+    }
+}
