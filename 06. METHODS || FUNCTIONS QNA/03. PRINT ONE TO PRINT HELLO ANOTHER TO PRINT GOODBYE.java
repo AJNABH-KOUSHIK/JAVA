f5c94f2452b3d872f2832abc69b89ca8489dda10
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-09 20:44:23 +0530

    03. PRINT ONE TO PRINT HELLO ANOTHER TO PRINT GOODBYE.java

---
+++ 06. METHODS || FUNCTIONS QNA/03. PRINT ONE TO PRINT HELLO ANOTHER TO PRINT GOODBYE.java
@@ -0,0 +1,15 @@
+//WRITE A CODE TO PRINT ONE TO PRINT HELLO ANOTHER TO PRINT GOODBYE
+public class TwoFunctions {
+    static void sayHello() {
+        System.out.println("Hello");
+    }
+
+    static void sayGoodbye() {
+        System.out.println("Goodbye");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+        sayGoodbye();
+    }
+}
