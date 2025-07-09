a225874a4e0b5669d16dc24233aa02dcb981cf36
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-09 21:01:02 +0530

    01. PRINT 3 TYPES OF MARKS .java

---
+++ 07. ARRAYS QNA/01. PRINT 3 TYPES OF MARKS .java
@@ -0,0 +1,9 @@
+//WRITE A CODE TO PRINT 3 TYPES OF MARKS USING ARRAYS
+public class Q33_PrintMarksArray {
+    public static void main(String[] args) {
+        int[] marks = {85, 90, 78}; // Example marks: Math, Physics, Chemistry
+        System.out.println("Math: " + marks[0]);
+        System.out.println("Physics: " + marks[1]);
+        System.out.println("Chemistry: " + marks[2]);
+    }
+}
