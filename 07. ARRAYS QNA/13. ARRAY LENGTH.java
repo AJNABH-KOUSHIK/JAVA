47a3f232f5c97982c52b0f1cfbb14d95bd0270ca
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-09 22:29:09 +0530

    13. ARRAY LENGTH.java

---
+++ 07. ARRAYS QNA/13. ARRAY LENGTH.java
@@ -0,0 +1,8 @@
+// ARRAY LENGTH
+public class ArrayLengthExample {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Length of the array: " + numbers.length);
+    }
+}
