4faa7bf6309bf40a4a78b6af80a3eb6ca71eb5a7
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-09 15:06:24 +0530

    01. PRINT 0 TO 100.java

---
+++ 05. LOOPS/01. PRINT 0 TO 100.java
@@ -0,0 +1,8 @@
+// WRITE A CODE TO PRINT 0 TO 100 USING LOOP
+public class Q1_Print0To100 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
