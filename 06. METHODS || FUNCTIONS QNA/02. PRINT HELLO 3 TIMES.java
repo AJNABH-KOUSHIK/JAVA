b39d5dc18739db72b3cd6dc1af76005f70aeecf6
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-09 20:43:03 +0530

    02. PRINT HELLO 3 TIMES.java

---
+++ 06. METHODS || FUNCTIONS QNA/02. PRINT HELLO 3 TIMES.java
@@ -0,0 +1,12 @@
+// WRITE A CODE TO PRINT HELLO 3 TIMES
+public class Hello3Times {
+    static void sayHello() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            sayHello();
+        }
+    }
+}
